8103aed6052a60b7c4cccefc433436686d6253cf
Wason <xxx>
2015-09-18 19:49:20 +0800

    d

--- Vetash/src/controller/ProductServlet.java
+++ Vetash/src/controller/ProductServlet.java
@@ -5,35 +5,15 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
-
-
-
-
-
-
-
-
-
 import misc.Parse;
 import model.ProductBean;
 import model.ProductService;
-
-
-
-
-
-
-
-
 import org.json.simple.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -43,10 +23,10 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ProductService service;
+	@Override
 	public void init() throws ServletException {
-		ServletContext application = this.getServletContext();
-		ApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(application);
-		service = (ProductService)context.getBean("ProductService");
+		service = new ProductService();
+		super.init();
 	}
 	protected void doGet(HttpServletRequest req, HttpServletResponse rsp) throws ServletException, IOException {
 
