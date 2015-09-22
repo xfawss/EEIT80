f09002f11c15e0a55fda415662f974c941fbd349
alex <youngalexha@gmail.com>
2015-09-22 15:37:36 +0800

    test

--- Vetash/src/controller/ProductServlet.java
+++ Vetash/src/controller/ProductServlet.java
@@ -22,18 +22,19 @@ import org.json.simple.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
-
 @WebServlet("/products")
 public class ProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ProductService service;
+
 	@Override
 	public void init() throws ServletException {
 		service = new ProductService();
 		super.init();
 	}
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse rsp) throws ServletException, IOException {
-		
+
 		rsp.setContentType("text/html;charset=UTF-8");
 		// System.out.println("get");
 
@@ -67,15 +68,15 @@ public class ProductServlet extends HttpServlet {
 
 					return;
 				}
-			}			
+			}
 			if (type != null) {
 				if (action.equals("select")) {
 					List<Map<String, Object>> result = service.selectByType(type);
 					jObj.put("results", result);
 					out.print(jObj);
 					return;
-				}				
-				
+				}
+
 			}
 			if (id != null) {
 				if (action.equals("select")) {
@@ -89,19 +90,19 @@ public class ProductServlet extends HttpServlet {
 					jObj.put("results", result);
 					out.print(jObj);
 					return;
-					
+
 				}
 			}
 		}
-			if (id == null) {
-				if (action.equals("select")) {
-					List<Map<String, Object>> result = service.getAll();
-					jObj.put("results", result);
-					out.print(jObj);
-					return;
-				}
+		if (id == null) {
+			if (action.equals("select")) {
+				List<Map<String, Object>> result = service.getAll();
+				jObj.put("results", result);
+				out.print(jObj);
+				return;
 			}
-		
+		}
+
 	}
 
 	// select by name
@@ -141,52 +142,49 @@ public class ProductServlet extends HttpServlet {
 		List<String> errors = new ArrayList<String>();
 		Map<String, Object> results = new HashMap<String, Object>();
 
-		if(action != null){
-			//判斷是哪個動作,
-			if(action.equals("insert")){
+		if (action != null) {
+			// 判斷是哪個動作,
+			if (action.equals("insert")) {
 				if (id == null || id.length() == 0) {
-					errors.add( "新增或修改時ID名稱為必填欄位，請輸入");					
+					errors.add("新增或修改時ID名稱為必填欄位，請輸入");
 				}
 				if (type == null || type.length() == 0) {
-					errors.add( "新增或修改時種類為必填欄位，請輸入");					
+					errors.add("新增或修改時種類為必填欄位，請輸入");
 				}
-				if (name== null || name.length() == 0) {
+				if (name == null || name.length() == 0) {
 					errors.add("新增或修改時品名為必填欄位，請輸入");
 				}
-				if (price== null || price.length() == 0) {	
-					
-					errors.add( "新增或修改時價格為必填欄位，請輸入");						
-				}				
-			}else if(action.equals("update")){
+				if (price == null || price.length() == 0) {
+
+					errors.add("新增或修改時價格為必填欄位，請輸入");
+				}
+			} else if (action.equals("update")) {
 				if (id == null || id.length() == 0) {
-					errors.add( "新增或修改時ID名稱為必填欄位，請輸入");					
-				}				
-				if (name== null || name.length() == 0) {
+					errors.add("新增或修改時ID名稱為必填欄位，請輸入");
+				}
+				if (name == null || name.length() == 0) {
 					errors.add("新增或修改時品名為必填欄位，請輸入");
 				}
-				if (price== null || price.length() == 0) {
-					errors.add( "新增或修改時價格為必填欄位，請輸入");					
-				}				
+				if (price == null || price.length() == 0) {
+					errors.add("新增或修改時價格為必填欄位，請輸入");
+				}
 			}
-			
-		}else{
+
+		} else {
 			return;
 		}
 
-		
-
 		// 轉換資料
-		
 
 		int parsePrice = 0;
-		int parseCost = 0;		
+		int parseCost = 0;
 		if (id != null && id.length() != 0)
 
 		{
-			parsePrice = Parse.convertInt(price);						
+			parsePrice = Parse.convertInt(price);
 		}
 
-		 System.out.println(jObj);
+		System.out.println(jObj);
 		if (errors != null && !errors.isEmpty()) {
 			req.getRequestDispatcher("/Wason/ProductTest.jsp").forward(req, rsp);
 			return;
@@ -198,10 +196,10 @@ public class ProductServlet extends HttpServlet {
 		bean.setProductType(type);
 		bean.setProductId(id);
 		bean.setProductName(name);
-		bean.setProductPrice(parsePrice);			
+		bean.setProductPrice(parsePrice);
 		bean.setProductImgPath(imgPath);
-		
-//		note,type,id,name,parsePrice,imgPath
+
+		// note,type,id,name,parsePrice,imgPath
 
 		// System.out.println(bean);
 
@@ -210,27 +208,27 @@ public class ProductServlet extends HttpServlet {
 		if (action != null && action.equals("insert"))
 
 		{
-			ProductBean result = service.insert(id,type,name,parsePrice,imgPath,note);
+			ProductBean result = service.insert(id, type, name, parsePrice, imgPath, note);
 			if (result == null) {
 				results.put("state", "新增失敗");
-				
+
 			} else {
 				req.setAttribute("insert", result);
 				results.put("state", "新增" + result + "筆成功");
-				
+
 			}
 
 		} else if (action != null && action.equals("update"))
 
 		{
-			ProductBean result = service.update(id,name,parsePrice,imgPath,note);
+			ProductBean result = service.update(id, name, parsePrice, imgPath, note);
 			if (result == null) {
 				results.put("state", "修改失敗");
-				
+
 			} else {
 				req.setAttribute("update", result);
 				results.put("state", "修改1筆成功");
-				
+
 			}
 
 			// System.out.println(action + "完成");
@@ -241,18 +239,18 @@ public class ProductServlet extends HttpServlet {
 			// System.out.println(result);
 			if (result == null) {
 				results.put("state", "刪除失敗");
-				
+
 			} else {
 				req.setAttribute("delete", result);
 				results.put("state", "刪除" + result + "筆成功");
-				
+
 			}
 		} else {
 			results.put("state", "不知道您現在要" + action + "什麼");
-			}
+		}
 		jObj.put("state", results);
 		out.print(jObj);
 		return;
-		
+
 	}
 }
